6c48fe69e0dc4132c79be26a3d150606bd4eebe7
usernamedt <usernamedt@protonmail.ch>
2021-02-11 15:50:07 +0500

    remove 'Q'

--- src/common/zpq_stream.c
+++ src/common/zpq_stream.c
@@ -120,7 +120,7 @@ struct ZpqStream
 static inline bool
 zpq_should_compress(char msg_type, uint32 msg_len)
 {
-	return msg_type == 'd' || msg_type == 'D' || msg_type == 'Q';
+	return msg_type == 'd' || msg_type == 'D';
 }
 
 /*
